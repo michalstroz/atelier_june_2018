a986a269eafe6df3dd2f4ff6093b7e70b5bd6f4e
Michał <michalstroz@gmail.com>
2018-06-20 20:10:36 +0200

    add new methods to mailer

--- app/mailers/books_notifier_mailer.rb
+++ app/mailers/books_notifier_mailer.rb
@@ -9,4 +9,22 @@ class BooksNotifierMailer < ApplicationMailer
 
     mail(to: user.email, subject: "Wypożyczyłeś książkę #{book.title}!")
   end
+
+  def book_return_reminder(book)
+    @book = book
+    @reservation = book.reservations.find_by(status: "TAKEN")
+    @borrower = @reservation.user
+
+    mail(to: @borrower.email, subject: "Upływa termin zwrotu książki #{@book.title}")
+  end
+
+  def book_reserved_return(book)
+    @book = book
+    @reservation = book.reservations.find_by(status: "RESERVED")
+    unless @reservation.nil?
+      @reserver = @reservation.user
+
+      mail(to: @reserver.email, subject: "Już niedługo będziesz mógł wypożyczyć książkę #{@book.title}")
+    end
+  end
 end
