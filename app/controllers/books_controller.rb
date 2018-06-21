f011cb1a3395ef652ea20e2772a9294ef0c43286
Michał <michalstroz@gmail.com>
2018-06-21 22:19:52 +0200

    add methods in BooksController: #filter which renders books/filter partial, #filter_books which delivers params of books matching to ours criteria, #permited_params, and #filter_params which return hash of params necessary to filter_books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,14 +3,14 @@ class BooksController < ApplicationController
   before_action :load_book, only: :show
   before_action :new_book, only: :create
 
-  def index
-  end
-
-  def new
-  end
-
-  def edit
-  end
+  # def index
+  # end
+  #
+  # def new
+  # end
+  #
+  # def edit
+  # end
 
   def create
     if new_book.save
@@ -20,13 +20,32 @@ class BooksController < ApplicationController
     end
   end
 
+  def filter
+    render template: 'books/filter', locals: { books: filter_books }
+  end
+
   def show
   end
 
-  def destroy
-  end
+  # def destroy
+  # end
 
   private
+
+  def filter_params
+    permited_params
+      .slice(:title, :isbn)
+      .merge(category.present? ? { category_id: category.id } : {})
+      .reject{ |k, v| v.to_s.empty? }
+  end
+
+  def filter_books
+    Book.where(filter_params)
+  end
+
+  def category
+    Category.find_by(name: permited_params[:category_name])
+  end
 
   def load_books
     @books = Book.all
@@ -38,5 +57,9 @@ class BooksController < ApplicationController
 
   def new_book
     @book = Book.new(title: params[:title], isbn: params[:isbn], category_name: params[:category_name])
+  end
+
+  def permited_params
+    params.permit(:title, :isbn, :category_id, :category_name)
   end
 end
