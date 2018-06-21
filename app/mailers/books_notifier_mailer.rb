4680cd0b34a28a3440cc790a35316f208b0529ec
Michał <michalstroz@gmail.com>
2018-06-22 00:36:09 +0200

    create book_reserverd_method function

--- app/mailers/books_notifier_mailer.rb
+++ app/mailers/books_notifier_mailer.rb
@@ -13,16 +13,17 @@ class BooksNotifierMailer < ApplicationMailer
   def book_return_reminder(book)
     @book = book
     @reservation = book.reservations.find_by(status: "TAKEN")
-    @borrower = @reservation.user
+    @borrower = @reservation.try(:user)
 
     mail(to: @borrower.email, subject: "Upływa termin zwrotu książki #{@book.title}")
   end
 
   def book_reserved_return(book)
     @book = book
-    @reservation = book.reservations.find_by(status: "RESERVED")
-    unless @reservation.nil?
-      @reserver = @reservation.user
+    @reservation = book.reservations.find_by(status: "TAKEN")
+    @reserver = book.reservations.where(status: "RESERVED").first.try(:user)
+
+    return if @reservation.blank? || @reserver.blank?
 
       mail(to: @reserver.email, subject: "Już niedługo będziesz mógł wypożyczyć książkę #{@book.title}")
     end
