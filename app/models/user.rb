44786b226414d9b6669a457eb849b76a9650c44d
Michał <michalstroz@gmail.com>
2018-07-03 00:25:46 +0200

    remove from_omniauth method from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,17 +6,5 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_many :reservations
-
-  def self.from_omniauth(access_token)
-   data = access_token.info
-   user = User.where(email: data['email']).first
-
-   unless user
-     user = User.create(
-      email: data['email'],
-      password: Devise.friendly_token[0,20]
-     )
-   end
-   user
- end
+  
 end
