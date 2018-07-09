7fd2da5abde1a76eed97e2f6a75f32ccd45d3349
Michał <michalstroz@gmail.com>
2018-07-09 21:13:55 +0200

    add new information to user in service

--- app/services/user_from_omniauth_getter.rb
+++ app/services/user_from_omniauth_getter.rb
@@ -4,15 +4,17 @@ class UserFromOmniauthGetter
   end
 
   def perform
-    data = @access_token.info
-    user = User.where(email: data['email']).first
-
-    unless user
-      user = User.create(
-        email: data['email'],
-        password: Devise.friendly_token[0,20]
-      )
+    User.where(provider: access_token.provider, uid: access_token.uid).first_or_create do |user|
+      user.provider = access_token.provider
+      user.uid = access_token.uid
+      user.email = access_token.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.token = access_token.credentials.token
+      user.refresh_token = access_token.credentials.refresh_token
     end
-    user
   end
+
+  private
+
+  attr_reader :access_token
 end
