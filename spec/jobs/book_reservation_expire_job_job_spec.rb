30b74ad505d1b389bcb8ebd0a24eed6af95d1570
Michał <michalstroz@gmail.com>
2018-08-30 21:48:04 +0200

    write the test for book_reservations_expire_job which is enqueued for date and time

--- spec/jobs/book_reservation_expire_job_job_spec.rb
+++ spec/jobs/book_reservation_expire_job_job_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe BookReservationExpireJobJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#perform_later" do
+    it "book reservation expire reminder" do
+      ActiveJob::Base.queue_adapter = :test
+      expect {
+        BookReservationExpireJobJob.set(wait_until: Date.tomorrow.to_s).perform_later('book')
+      }.to have_enqueued_job.with('book').at(Date.tomorrow.to_s)
+    end
+  end
 end
