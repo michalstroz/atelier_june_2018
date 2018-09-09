e2e78af8b30c262ca82613fefa9338367d4e0e61
Michał <michalstroz@gmail.com>
2018-09-09 21:53:19 +0200

    add fb provider to the model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, omniauth_providers: [:google_oauth2]
+         :omniauthable, omniauth_providers: [:google_oauth2, :facebook]
 
   has_many :reservations
-  
+
 end
