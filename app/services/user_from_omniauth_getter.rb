d1151a5ba75f0cc227a09538dcf236837e43015b
Michał <michalstroz@gmail.com>
2018-09-09 22:06:37 +0200

    add fb authorization to User model

--- app/services/user_from_omniauth_getter.rb
+++ app/services/user_from_omniauth_getter.rb
@@ -14,6 +14,16 @@ class UserFromOmniauthGetter
     end
   end
 
+  def fb_perform
+    User.where(provider: access_token.provider, uid: access_token.uid).first_or_create do |user|
+      user.email = access_token.info.email
+      user.password = Devise.friendly_token[0,20]
+      # If you are using confirmable and the provider(s) you use validate emails,
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
+  end
+
   private
 
   attr_reader :access_token
